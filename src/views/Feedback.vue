d547c50f8d00191a1013efd83228ee716b238f05
BecaLHughes <becalhughes@gmail.com>
2022-05-03 20:47:12 +0100

    changes to feedback page

--- src/views/Feedback.vue
+++ src/views/Feedback.vue
@@ -28,7 +28,7 @@
               <v-list-item-icon>
                 <v-icon>mdi-logout</v-icon>
               </v-list-item-icon>
-              <v-list-item-title><router-link to="/profile">Logout</router-link></v-list-item-title>
+              <v-list-item-title><router-link to="/signin">Logout</router-link></v-list-item-title>
             </v-list-item>
 
           </v-list-item-group>
@@ -38,12 +38,30 @@
       <div class="pa-md-10 mx-lg-auto">
           <h1 align="center">Your Score</h1>
       </div>
-      <div class="text-center">
+      <div class="text-center mb-6">
         <v-progress-circular :rotate="-90" :size="300" :width="15" :value="score.value" color="teal">
-      <h1 class="score">{{ score.value }}</h1>
-    </v-progress-circular>
-
+          <h1 class="score">{{ score.value }}</h1>
+        </v-progress-circular>
       </div>
+      <div class="pl-3 pr-3">
+      <v-alert
+          border="top"
+          color="blue lighten-2"
+          dark
+          v-for="e in errors" :key="e.question"
+        >
+          <h4>{{ e.question }}</h4>
+          <v-chip
+            class="mt-2 mb-3"
+            color="blue"
+            label
+          >
+            Score: {{ e.score}}
+          </v-chip>
+          <h2>Tip</h2>
+          <p>{{ e.tip }}</p>
+        </v-alert>
+        </div>
   </v-card>
 </div>
 </template>
@@ -58,7 +76,13 @@ export default {
           value: 0,
         },
         drawer: false,
-        group: null
+        group: null,
+        errors: [ {
+          question: 'Lorem Ipsum',
+          score: 0,
+          tip: 'Lorem ipsum'
+        }]
+
       }
   },
   beforeDestroy () {
